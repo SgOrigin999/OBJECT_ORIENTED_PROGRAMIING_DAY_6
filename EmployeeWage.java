8f6b40522be0dc853fe2b19475c4e15d32c75907
SgOrigin999 <you@example.com>
2021-06-01 17:28:02 +0530

    Employe Avilable or Not

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,22 @@
 package com.bridgelab.employeewage.day6;
 
 public class EmployeeWage {
-public static void main(String[] args) {
-	System.out.println("Welcome to Main Branch of Day Six::");
-}
+	public static final int FULL_TIME = 1;
+
+	public void checkAvilablity() {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		System.out.println("Emp Check Value::" + empCheck);
+		if (empCheck == FULL_TIME) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is not Present");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Main Branch of Day Six::");
+		EmployeeWage emp = new EmployeeWage();
+		System.out.println("Check Employee Avilablity::");
+		emp.checkAvilablity();
+	}
 }
